68c1cf7442e1a3e15249efbcc3a84de24f8891d2
unknown <sreelakshmim997@gmail.com>
2023-01-29 16:48:32 +0530

    First commit

---
+++ WebApplication1/Controllers/HelloworldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloworldController : Controller
+    {
+
+        private static List<Family> Fam = new List<Family>();
+
+        public IActionResult Index()
+        {
+            return View(Fam);
+        }
+
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Created(Family fam)
+        {
+            Fam.Add(fam);
+            var a = Fam.FirstOrDefault();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
